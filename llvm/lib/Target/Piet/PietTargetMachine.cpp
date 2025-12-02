d1268b9bea2d13e475824a44d1d37ede14b3dc16
Lasse <lasse@letager.dk>
2025-12-02 02:43:05 +0100

    WIP

--- llvm/lib/Target/Piet/PietTargetMachine.cpp
+++ llvm/lib/Target/Piet/PietTargetMachine.cpp
@@ -1,3 +1,6 @@
-extern "C" void LLVMInitializePietTarget() {
-    RegisterTargetMachine<PietTargetMachine> X(getThePietTarget());
-}
+// extern "C" void LLVMInitializePietTarget() {
+//     RegisterTargetMachine<PietTargetMachine> X(getThePietTarget());
+// }
+
+extern "C" void LLVMInitializePietTargetInfo();
+extern "C" void LLVMInitializePietTargetMC();
